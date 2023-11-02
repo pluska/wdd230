b370219c859942a2265c8ef863f9c5e6d95c3173
Andres Pluska <pluskaandres@gmail.com>
2023-11-02 13:03:46 -0500

    Bugfixing

--- chamber/scripts/burguer-menu.js
+++ chamber/scripts/burguer-menu.js
@@ -1,14 +1,14 @@
 const button = document.querySelector('#menu-button');
-const sidebarBtn = document.querySelector('#sidebar-menu');
 const navigation = document.querySelector('.navigation');
-const sidebar = document.querySelector('.sidebar');
-
 button.addEventListener('click', () => {
   navigation.classList.toggle('open');
   button.classList.toggle('open');
 })
-
-sidebarBtn.addEventListener('click', () => {
-  sidebar.classList.toggle('open');
-  sidebarBtn.classList.toggle('open');
-})
+if (window.location.pathname === '/chamber/discover.html') {
+  const sidebarBtn = document.querySelector('#sidebar-menu') ;
+  const sidebar = document.querySelector('.sidebar');
+  sidebarBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('open');
+    sidebarBtn.classList.toggle('open');
+  })
+}
