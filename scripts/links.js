340e267015195fc3f728dfe24d42965ffe3d9ba4
Andres Pluska <pluskaandres@gmail.com>
2023-11-15 01:48:44 -0500

    Fixing CSS bug

--- scripts/links.js
+++ scripts/links.js
@@ -11,16 +11,31 @@ const getLinks = async () => {
 const displayLinks = (weeks) => {
   const ul = document.querySelector("#activities");
   weeks.lessons.forEach((week) => {
-    week.links.forEach((link) => {
+    if (week.links.length > 1) {
       const li = document.createElement("li");
-      const a = document.createElement("a");
-      a.href = link.url;
-      a.target = "_blank";
-      a.textContent = link.title;
       li.textContent = `Week ${week.lesson}: `;
-      li.appendChild(a);
+      week.links.forEach((link, index) => {
+        const a = document.createElement("a");
+        a.href = link.url;
+        a.target = "_blank";
+        a.textContent = link.title;
+        li.appendChild(a);
+        if (index < week.links.length - 1)
+        li.textContent += " | ";
+      })
       ul.appendChild(li);
-    });
+    } else {
+      week.links.forEach((link) => {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        a.href = link.url;
+        a.target = "_blank";
+        a.textContent = link.title;
+        li.textContent = `Week ${week.lesson}: `;
+        li.appendChild(a);
+        ul.appendChild(li);
+      });
+    }
   });
 }
 
